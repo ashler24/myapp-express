b504a9294925ff54dbf1c3667d3e6d5bd1192f21
Abhijeet <pawar2467@gmail.com>
2019-11-28 17:30:17 +0530

    added test.sh

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+npm install
+
+pm2 describe server > /dev/null
+
+RUNNING=$?
+
+if [ "{RUNNING}" -ne 0 ];
+then
+    echo "server not running.... start"
+    pm2 start server
+else
+    echo "server running.... restart"
+    pm2 restart server
+fi
